fd6e19dcd60beee178da0e6ec93b0e265e0bb98b
zengchen1024 <chenzeng2@huawei.com>
2017-12-11 21:03:28 +0800

    add project id to provider client

--- vendor/github.com/gophercloud/gophercloud/openstack/client.go
+++ vendor/github.com/gophercloud/gophercloud/openstack/client.go
@@ -164,6 +164,7 @@ func v2auth(client *gophercloud.ProviderClient, endpoint string, options gopherc
 		}
 	}
 	client.TokenID = token.ID
+	client.ProjectID = token.Tenant.ID
 	client.EndpointLocator = func(opts gophercloud.EndpointOpts) (string, error) {
 		return V2EndpointURL(catalog, opts)
 	}
@@ -194,12 +195,18 @@ func v3auth(client *gophercloud.ProviderClient, endpoint string, opts tokens3.Au
 		return err
 	}
 
+	project, err := result.ExtractProject()
+	if err != nil {
+		return err
+	}
+
 	catalog, err := result.ExtractServiceCatalog()
 	if err != nil {
 		return err
 	}
 
 	client.TokenID = token.ID
+	client.ProjectID = project.ID
 
 	if opts.CanReauth() {
 		client.ReauthFunc = func() error {
