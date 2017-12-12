01ec6532c3c1c36c988586a9956e3cc766225515
huangtianhua <huangtianhua@huawei.com>
2017-12-12 09:38:43 +0800

    Implement as group resource management

--- openstack/provider.go
+++ openstack/provider.go
@@ -204,6 +204,7 @@ func Provider() terraform.ResourceProvider {
 			"openstack_networking_secgroup_rule_v2":     resourceNetworkingSecGroupRuleV2(),
 			"openstack_objectstorage_container_v1":      resourceObjectStorageContainerV1(),
 			"openstack_objectstorage_object_v1":         resourceObjectStorageObjectV1(),
+			"openstack_as_group_v1":                     resourceASGroup(),
 		},
 
 		ConfigureFunc: configureProvider,
