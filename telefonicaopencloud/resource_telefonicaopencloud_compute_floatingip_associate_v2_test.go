4ce240b73151e989b6b97c6302cf5a2a62b66b8e
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2017-12-29 10:32:25 +0800

    Use OS_NETWORK_ID as default network in Acc tests
    If there are multiple private networks can be used in current tenant,
    that causes booting instance failed, because Nova have no idea about
    using which network to create instance.

--- telefonicaopencloud/resource_telefonicaopencloud_compute_floatingip_associate_v2_test.go
+++ telefonicaopencloud/resource_telefonicaopencloud_compute_floatingip_associate_v2_test.go
@@ -199,10 +199,12 @@ func testAccCheckComputeV2FloatingIPAssociateAssociated(
 	}
 }
 
-const testAccComputeV2FloatingIPAssociate_basic = `
+var testAccComputeV2FloatingIPAssociate_basic = fmt.Sprintf(`
 resource "telefonicaopencloud_compute_instance_v2" "instance_1" {
   name = "instance_1"
   security_groups = ["default"]
+  network {
+    uuid = "%s"
 }
 
 resource "telefonicaopencloud_networking_floatingip_v2" "fip_1" {
@@ -212,12 +214,15 @@ resource "telefonicaopencloud_compute_floatingip_associate_v2" "fip_1" {
   floating_ip = "${telefonicaopencloud_networking_floatingip_v2.fip_1.address}"
   instance_id = "${telefonicaopencloud_compute_instance_v2.instance_1.id}"
 }
-`
+`, OS_NETWORK_ID)
 
-const testAccComputeV2FloatingIPAssociate_fixedIP = `
+var testAccComputeV2FloatingIPAssociate_fixedIP = fmt.Sprintf(`
 resource "telefonicaopencloud_compute_instance_v2" "instance_1" {
   name = "instance_1"
   security_groups = ["default"]
+  network {
+    uuid = "%s"
+  }
 }
 
 resource "telefonicaopencloud_networking_floatingip_v2" "fip_1" {
@@ -228,7 +233,7 @@ resource "telefonicaopencloud_compute_floatingip_associate_v2" "fip_1" {
   instance_id = "${telefonicaopencloud_compute_instance_v2.instance_1.id}"
   fixed_ip = "${telefonicaopencloud_compute_instance_v2.instance_1.access_ip_v4}"
 }
-`
+`, OS_NETWORK_ID)
 
 var testAccComputeV2FloatingIPAssociate_attachToFirstNetwork = fmt.Sprintf(`
 resource "telefonicaopencloud_compute_instance_v2" "instance_1" {
@@ -287,10 +292,13 @@ resource "telefonicaopencloud_compute_floatingip_associate_v2" "fip_1" {
 }
 `, OS_NETWORK_ID)
 
-const testAccComputeV2FloatingIPAssociate_attachNew_1 = `
+var testAccComputeV2FloatingIPAssociate_attachNew_1 = fmt.Sprintf(`
 resource "telefonicaopencloud_compute_instance_v2" "instance_1" {
   name = "instance_1"
   security_groups = ["default"]
+  network {
+    uuid = "%s"
+  }
 }
 
 resource "telefonicaopencloud_networking_floatingip_v2" "fip_1" {
@@ -303,12 +311,15 @@ resource "telefonicaopencloud_compute_floatingip_associate_v2" "fip_1" {
   floating_ip = "${telefonicaopencloud_networking_floatingip_v2.fip_1.address}"
   instance_id = "${telefonicaopencloud_compute_instance_v2.instance_1.id}"
 }
-`
+`, OS_NETWORK_ID)
 
-const testAccComputeV2FloatingIPAssociate_attachNew_2 = `
+var testAccComputeV2FloatingIPAssociate_attachNew_2 = fmt.Sprintf(`
 resource "telefonicaopencloud_compute_instance_v2" "instance_1" {
   name = "instance_1"
   security_groups = ["default"]
+  network {
+    uuid = "%s"
+  }
 }
 
 resource "telefonicaopencloud_networking_floatingip_v2" "fip_1" {
@@ -321,4 +332,4 @@ resource "telefonicaopencloud_compute_floatingip_associate_v2" "fip_1" {
   floating_ip = "${telefonicaopencloud_networking_floatingip_v2.fip_2.address}"
   instance_id = "${telefonicaopencloud_compute_instance_v2.instance_1.id}"
 }
-`
+`, OS_NETWORK_ID)
