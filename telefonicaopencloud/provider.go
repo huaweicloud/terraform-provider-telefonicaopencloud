cc4f2f408ae7e9b59e4e5ad6b3791f570f7255c5
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2018-02-09 11:02:48 +0800

    Remove deprecated volume v1 support

--- telefonicaopencloud/provider.go
+++ telefonicaopencloud/provider.go
@@ -166,7 +166,6 @@ func Provider() terraform.ResourceProvider {
 		},
 
 		ResourcesMap: map[string]*schema.Resource{
-			"telefonicaopencloud_blockstorage_volume_v1":          resourceBlockStorageVolumeV1(),
 			"telefonicaopencloud_blockstorage_volume_v2":          resourceBlockStorageVolumeV2(),
 			"telefonicaopencloud_compute_flavor_v2":               resourceComputeFlavorV2(),
 			"telefonicaopencloud_compute_instance_v2":             resourceComputeInstanceV2(),
